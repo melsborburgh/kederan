f33e88755322c879d7c79d812fa27648f1661206
Mels Borburgh <mbrb@axi.nl>
2019-10-08 12:16:25 +0200

    Exported tables DDL

--- src/main/database/install/tables/kdr_plot_npc.sql
+++
@@ -1,8 +0,0 @@
-create table kdr_plot_npcs
-(
-    NPC_ID              NUMBER not null enable,
-    PLOT_ID             NUMBER not null enable,
-    CONSTRAINT kdr_plot_npcs_pk PRIMARY KEY (NPC_ID, PLOT_ID),
-    CONSTRAINT kdr_plot_npcs_plot_id_fk FOREIGN KEY (plot_id) REFERENCES kdr_plot(plot_id) ON DELETE CASCADE,
-    CONSTRAINT kdr_plot_npcs_npc_id_fk FOREIGN KEY (npc_id) REFERENCES kdr_npcs(npc_id) ON DELETE CASCADE
-);
